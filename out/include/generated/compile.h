ac4a9dd6aaae754ea8213b3e8c293a218a843bd8
raghavt20 <raghavtandon2000@gmail.com>
2021-11-18 18:33:24 +0530

    ARM64: configs: vayu: Rebrand to Litten
    Change-Id: I04f3eeced138131128f445374d6921ba634c3bf5

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Sun Jul 21 12:22:42 IST 2024"
+#define LINUX_COMPILE_BY "nobody"
+#define LINUX_COMPILE_HOST "android-build"
+#define LINUX_COMPILER "Android (10087095, +pgo, +bolt, +lto, -mlgo, based on r487747c) clang version 17.0.2 (https://android.googlesource.com/toolchain/llvm-project d9f89f4d16663d5012e5c09495f3b30ece3d2362), LLD 17.0.2"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.14.348-openela-Litten-openela-gd533506f1fc7-dirty"
